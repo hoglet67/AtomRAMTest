d671be43952273e8e1c09779e4ccc36d53f23d38
David Banks <dave@hoglet.com>
2022-04-20 20:06:29 +0100

    Output free space and md5sum

--- build.sh
+++ build.sh
@@ -6,12 +6,14 @@ rm -f build/*
 for i in 82 A0 D0 E0 F0
 do
     beebasm -D test_start=\&${i}00 -v -i src/RAMTest.asm -o build/RAMTST${i} > build/RAMTST${i}.log
-    dd if=build/RAMTST${i} of=build/RAMTST${i}.rom bs=1 skip=22
+    tail -c +23 <build/RAMTST${i} >build/RAMTST${i}.rom
+    grep -i "Free Space" build/RAMTST${i}.log
+    md5sum build/RAMTST${i}.rom
 done
 
 # Build a special; test version that runs in CLEAR 4
 beebasm -D test_start=\&2900 -D screen_base=\&4000 -D screen_init=\&F0 -D page_start=\&80 -D page_end=\&97 -v -i src/RAMTest.asm -o build/RAMTST29 > build/RAMTST29.log
-dd if=build/RAMTST29 of=build/RAMTST29.rom bs=1 skip=22
+tail -c +23 <build/RAMTST29 >build/RAMTST29.rom
 
 ls -l build
 
